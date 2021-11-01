502f4bfa9547fc6b15a36c856fd3483a68856c4e
GUKOVPC\Gukov <armor.1215@gmail.com>
2021-11-01 16:36:21 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const TodoaApp());
+}
+
+class TodoaApp extends StatelessWidget {
+  const TodoaApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TodoaApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TodoaApp'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                child: const Image(
+                  image: AssetImage('assets/images/bg_top_image.png'),
+                ),
+              ),
+            ),
+            ListView.builder(
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return Row(
+                  children: [
+                    Checkbox(
+                      value: true,
+                      onChanged: (bool? value) {},
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(12.0),
+                      width: 50.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: const Color(0xff7c94b6),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(50.0)),
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 4.0,
+                        ),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 4.0), //(x,y)
+                            blurRadius: 4.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 12.0, vertical: 0.0),
+                      child: const Text(
+                        'Item Text',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
