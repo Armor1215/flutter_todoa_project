5dc263d0cf6cf77f0ba3a992ea1c4a805ae502ec
GUKOVPC\Gukov <armor.1215@gmail.com>
2021-11-04 19:32:35 +0200

    Changes in class HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class TodoaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'TodoaApp',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -27,62 +28,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('TodoaApp'),
-      ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Center(
-              child: Container(
-                child: const Image(
-                  image: AssetImage('assets/images/bg_top_image.png'),
+        child: ListView.builder(
+          itemCount: 4,
+          itemBuilder: (context, index) {
+            return Row(
+              children: [
+                Checkbox(
+                  value: true,
+                  onChanged: (bool? value) {},
                 ),
-              ),
-            ),
-            ListView.builder(
-              itemCount: 4,
-              itemBuilder: (context, index) {
-                return Row(
-                  children: [
-                    Checkbox(
-                      value: true,
-                      onChanged: (bool? value) {},
+                Container(
+                  margin: const EdgeInsets.all(12.0),
+                  width: 50.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                    color: const Color(0xff7c94b6),
+                    borderRadius: const BorderRadius.all(Radius.circular(50.0)),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 4.0,
                     ),
-                    Container(
-                      margin: const EdgeInsets.all(12.0),
-                      width: 50.0,
-                      height: 50.0,
-                      decoration: BoxDecoration(
-                        color: const Color(0xff7c94b6),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(50.0)),
-                        border: Border.all(
-                          color: Colors.white,
-                          width: 4.0,
-                        ),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.grey,
-                            offset: Offset(0.0, 4.0), //(x,y)
-                            blurRadius: 4.0,
-                          ),
-                        ],
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0, 4.0), //(x,y)
+                        blurRadius: 4.0,
                       ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 12.0, vertical: 0.0),
-                      child: const Text(
-                        'Item Text',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                    ),
-                  ],
-                );
-              },
-            ),
-          ],
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                      horizontal: 12.0, vertical: 0.0),
+                  child: const Text(
+                    'Item Text',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
